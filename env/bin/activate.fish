489bcb7e631d28f2ef6ca42178b5f755db191358
bfiguet <blandinefiguet@gmail.com>
2024-04-17 18:09:41 +0200

    python manage.py runserver

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/mnt/nfs/homes/bfiguet/Desktop/siteE-commerce/env"
+set -gx VIRTUAL_ENV "/mnt/nfs/homes/bfiguet/Desktop/site_E-Commerce/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
